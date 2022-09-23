c9a921cc0c11308dffe37746646236a64754c457
yuanenzhi <710575900@qq.com>
2022-09-23 14:46:45 +0800

    .

--- src/main/java/com/example/demo/converter/JavaTableConverter.java
+++ src/main/java/com/example/demo/converter/JavaTableConverter.java
@@ -1,7 +1,11 @@
 package com.example.demo.converter;
 
+import com.example.demo.entity.JavaTable;
 import org.mapstruct.Mapper;
-
+import com.example.demo.entity.dto.JavaTableDTO;
 @Mapper(componentModel = "spring")
 public interface JavaTableConverter {
+    JavaTableDTO poToDto(JavaTable javaTable);
+
+    JavaTable dtoToPo(JavaTableDTO javaTableDTO);
 }
--- src/main/kotlin/com/example/demo/converter/KotlinTableConverter.kt
+++ src/main/kotlin/com/example/demo/converter/KotlinTableConverter.kt
@@ -1,7 +1,11 @@
 package com.example.demo.converter
 
+import com.example.demo.entity.JavaTable
+import com.example.demo.entity.dto.JavaTableDTO
 import org.mapstruct.Mapper
 
 @Mapper(componentModel = "spring")
 interface KotlinTableConverter {
+    fun poToDto(javaTable: JavaTable): JavaTableDTO
+    fun dtoToPo(javaTableDTO: JavaTableDTO): JavaTable
 }
