7cd1c3318c744b48db1645b11ec4dfd7a9e10e64
yuanenzhi <710575900@qq.com>
2022-09-29 12:30:28 +0800

    .

--- src/main/java/com/example/demo/entity/dto/JavaGoodsStockAddDTO.java
+++ src/main/java/com/example/demo/entity/dto/JavaGoodsStockAddDTO.java
@@ -1,14 +1,21 @@
 package com.example.demo.entity.dto;
 
 import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 /**
  * 商品库存
  */
 @Data
 public class JavaGoodsStockAddDTO {
+    @NonNull
     private Long goodsId;
+    @NotBlank
     private String name;
+    @Min(1)
     private Integer num;
 
 
